b89d9788d834247aa28165c722460daf03f827bd
gxngennie-gif <gxngennie@gmail.com>
2026-02-24 19:45:13 +0530

    Add files via upload

--- auto_rep_gen.py
+++ auto_rep_gen.py
@@ -1,5 +1,63 @@
+# import pandas as pd
+# from reportlab.platypus import SimpleDocTemplate,Paragraph,Spacer,Table,TableStyle
+# from reportlab.lib import colors
+# from reportlab.lib.styles import ParagraphStyle
+# from reportlab.platypus import SimpleDocTemplate
+# from reportlab.lib import colors
+# from reportlab.platypus import Table
+# from reportlab.lib.styles import getSampleStyleSheet
+# from reportlab.platypus import Paragraph
+# from reportlab.platypus import Spacer
+# from reportlab.platypus import TableStyle
+# from reportlab.pdfbase.ttfonts import TTFont
+# from reportlab.pdfbase import pdfmetrics
+# from reportlab.lib import colors
+# from reportlab.lib.pagesizes import A4
+# from reportlab.platypus import SimpleDocTemplate
+# from reportlab.platypus import Paragraph
+# from reportlab.platypus import Spacer
 
+# #read data
+# data=pd.read_csv("data.csv")
 
+# #analysing data
+# total_employees=len(data)
+# average_salary=data["Salary"].mean()
+# max_salary=data["Salary"].max()
+# min_salary=data["Salary"].min()
+
+# department_summary=data.groupby("Department")["Salary"].mean().reset_index
+
+# #create pdf
+# pdf=SimpleDocTemplate("employees_report.pdf",pagesizes=A4)
+# element=[]
+# style=getSampleStyleSheet()
+# element.append(Paragraph("employees salary report",style["Heading 1"]))
+# element.append(Spacer(1,20))
+
+# #summary
+# element.append(Paragraph(f"total employee salary:{total_employees}",style["normal"]))
+# element.append(Paragraph(f"average salary:${average_salary:.2f}",style["normal"]))
+# element.append(Paragraph(f"highest salary:${max_salary}",style["normal"]))
+# element.append(Paragraph(f"lowest salary:${min_salary}",style["normal"]))
+
+# #table section
+# table_data=[["Department","average salary"]]
+# for index, row in department_summary.iterrows():
+#     table=table(table_data)
+#     table.setstyle(TableStyle(
+#         [
+#            ('BACKGROUND',(0,0),(-1,0),colors.gray),
+#            ('TEXTCOLOR',(0,0),(-1,0),colors.whitesmoke),
+#            ('ALIGN',(0,0),(-1,-1),"CENTER"),
+#            ('GRID',(0,0),(-1,-1),1,colors.black) 
+#         ]
+#     ))
+
+#     element.append(table)
+#     pdf.build(element)
+
+#     print("pdf report generates successfully")
 import pandas as pd
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle
 from reportlab.lib import colors
